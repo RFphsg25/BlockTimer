64cc919a0913253d525ec939692dcefd4d28e61c
RFphsg25 <reto.frischknecht@student.phsg.ch>
2025-12-13 15:04:55 +0100

    Revert "Creation and extraction of countdown function"
    This reverts commit 6529e450edbf1dd2332671850836a09325d2ec23.
    
    # Conflicts:
    #	countdown.py

--- main.py
+++ main.py
@@ -1,37 +1,65 @@
 import py5
-from countdown import countdown
 
-# ==== Input Variablen ==== #
 std = 0
 m = 0
-sk = 30
-timerstd = 0
-timerm = 0
-timersk = 0
+sk = 5
+
+timerstd = std
+timerm = m
+timersk = sk
+last_sk = 0
 
 def settings():
-    py5.size(int(py5.display_width * 0.7), int(py5.display_height * 0.7))
+    w = py5.display_width
+    h = py5.display_height
+    py5.size(int(w * 0.7), int(h * 0.7))
 
 def setup():
-    global timerstd, timerm, timersk
-
-    timerstd = std
-    timerm = m
-    timersk = sk
 
     print("Initializing...")
     print(py5.get_frame_rate())
 
 def draw():
-    global std, m, sk, timerstd, timerm, timersk
-
-    py5.background(200, 200, 200)
-    py5.text_size(15)
-    py5.fill(0, 0, 0)
-    py5.text('Hello.', 50, py5.height / 2)
+    global std, m, sk, timersk, timerm, timerstd, last_sk
 
     # ==== Erstellt den Zeitablauf ==== #
     timersk -= 1/py5.get_frame_rate()
-    countdown(std, m, sk, timerstd, timerm, timersk)
+
+    if timersk < 0:
+        if timerstd > 0 or timerm > 0:
+            timersk = 60 + timersk
+            timerm -= 1
+        else:
+            timersk = 0
+
+    if timerm < 0:
+        if timerstd > 0:
+            timerm = 59
+            timerstd -= 1
+        else:
+            timerm = 0
+    if timerstd < 0:
+        timerstd = 0
+        timerm = 0
+        timersk = 0
+
+    total_sk = (std * 60 * 60) + (m * 60) + sk
+    remain_sk = (timerstd * 60 * 60) + (timerm * 60) + timersk
+    progress = 1 - (remain_sk / total_sk)
+    a = progress * py5.width
+    py5.background(200,200,200)
+    py5.text_size(15)
+    py5.fill(0, 0, 0)
+    py5.text('Hello, this is a functional script.', 50, py5.height / 2)
+    if a < py5.width:
+        py5.no_stroke()
+        py5.fill(200, 90, 100, 80)
+        py5.rect(0,py5.height-(py5.height*0.6), a, py5.height*0.6)
+        py5.fill(0,0,0)
+        py5.text(str(int(timerstd)) + " :Stunden " + str(int(timerm)) + ' :Minuten ' + str(int(timersk)) + ' :Sekunden', 50, py5.height / 3)
+    else:
+        py5.fill(200, 90, 100, 80)
+        py5.rect(0,py5.height-(py5.height*0.6),py5.width, py5.height*0.6)
+
 
 py5.run_sketch()
